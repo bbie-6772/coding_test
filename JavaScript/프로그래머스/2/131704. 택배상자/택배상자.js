7bb08faba9b58b4325f9d09ab644b27552263953
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-01-15 09:31:37 +0900

    [level 2] Title: 택배상자, Time: 40.91 ms, Memory: 53.7 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/131704. 택배상자/택배상자.js
+++ JavaScript/프로그래머스/2/131704. 택배상자/택배상자.js
@@ -1,18 +1,4 @@
 function solution(order) {
-    let answer = 0;
-    let max = 0;
-    let stack = [];
-    
-    for (let i=0;i < order.length;i++) { 
-        if(order[i] > max) {
-            for (let j = max + 1;j < order[i];j++) stack.push(j)
-            max = order[i]
-            answer++
-        } else if (order[i] === stack[stack.length-1]){
-            stack.pop()
-            answer++
-        } else break 
-    }
-    
+    var answer = 0;
     return answer;
 }
