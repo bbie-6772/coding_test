72ce75e98ae5840b130f438c2bfb830f1e41aa8c
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-02-27 09:30:57 +0900

    [level 2] Title: 과제 진행하기, Time: 32.84 ms, Memory: 38.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/176962. 과제 진행하기/과제 진행하기.js
+++ JavaScript/프로그래머스/2/176962. 과제 진행하기/과제 진행하기.js
@@ -1,54 +1,46 @@
 function solution(plans) {
     var answer = [];
     const stack = [];
-    let currentTime = 0;
+    
+    // 시간 변환 함수
+    const convertTimeToMinutes = (time) => {  
+        const [hour, minute] = time.split(":");  
+        return +hour * 60 + +minute;  
+    };  
     
     // [1] 시간 순서대로 정렬해서 시작
-    plans.sort((a,b) => {
-        let [ aHour, aMinutes] = a[1].split(":")
-        aMinutes = +aMinutes + +aHour * 60
-        let [ bHour, bMinutes]  = b[1].split(":")
-        bMinutes = +bMinutes + +bHour * 60
-        return aMinutes - bMinutes
-    })
+    plans.sort((a,b) => convertTimeToMinutes(a[1]) - convertTimeToMinutes(b[1]))
     
     // [2] 다음 시간과의 차이(분) 을 확인
     for (let i = 0; i < plans.length;i++) {
         const [name, start, duration] = plans[i]
-        const [ hour, minutes] = start.split(":")
-        const currentTime = +minutes + +hour * 60
+        const currentTime = convertTimeToMinutes(start)
         
         if(i === plans.length-1) {
             answer.push(name)
-            while(stack.length > 0) {
-                const [ nName, nDuration ] =stack.pop();
-                answer.push(nName)
-            }
+            while(stack.length > 0) answer.push(stack.pop()[0]);  
             break
         }
         
         const [nextN, nextS, nextD] = plans[i+1]
-        const [ nHour, nMinutes] = nextS.split(":")
-        const nextTime = +nMinutes + +nHour * 60
+        const nextTime = convertTimeToMinutes(nextS)
     
-        let time = nextTime - currentTime - +duration
+        let remainingTime = nextTime - currentTime - +duration
         // [3] 빨리 끝났다면 차이에서 그만큼을 제외하고 대기열 확인
-        if (time === 0) answer.push(name)
-        else if (time > 0) {
+        if (remainingTime >= 0) {
             answer.push(name)
-            while (stack.length > 0) {
-                let [nName, nDuration] = stack.pop();
-                const result = time - nDuration
-                if(result < 0) {
-                    nDuration -= time
-                    stack.push([nName, nDuration])
-                    break 
-                } 
-                answer.push(nName)
-                time = result
+            while (stack.length > 0 && remainingTime > 0) {
+                const [stackName, stackDuration] = stack.pop();
+                if( remainingTime >= stackDuration ) {
+                    answer.push(stackName)
+                    remainingTime -= stackDuration
+                } else {
+                    stack.push([stackName, stackDuration - remainingTime])
+                    break
+                }
             }
         // [4] 만약 끝나지 않았다면 그만큼을 제외하고 대기열에 넣기
-        } else if (time < 0) stack.push([name, -time])
+        } else stack.push([name, -remainingTime])
     }
     
     return answer;
