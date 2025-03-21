474b2941a7cd5cde66a4da2653f7f206c846494b
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-03-21 15:11:53 +0900

    Time: 1124 ms (95.01%), Space: 0B (100%) - LeetHub

--- Leetcode/1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ Leetcode/1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -4,10 +4,9 @@ WITH UserCount AS (
     FROM    Users
 )
 SELECT  R.contest_id 
-        , ROUND( COUNT(R.user_id) / user_count * 100,2) AS percentage
+        , ROUND( COUNT(R.user_id) / (SELECT user_count FROM UserCount) * 100,2) AS percentage
 FROM    Users U   
     INNER JOIN Register R
         ON U.user_id = R.user_id
-    CROSS JOIN  UserCount
 GROUP BY R.contest_id
 ORDER BY percentage DESC, R.contest_id ASC
