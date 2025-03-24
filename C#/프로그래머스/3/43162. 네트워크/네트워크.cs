5521f1793528f4fa21710e50e8aa3fcc0c4691ee
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-03-24 13:46:30 +0900

    [level 3] Title: 네트워크, Time: 12.81 ms, Memory: 32.9 MB -BaekjoonHub

---
+++ C#/프로그래머스/3/43162. 네트워크/네트워크.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Solution {
+    public int solution(int n, int[,] computers) {
+        int answer = 0;
+        
+        for (int i = 0; i < computers.GetLength(0);i++) 
+        {    
+            int[] row = new int[computers.GetLength(1)];  
+            for (int j = 0; j < computers.GetLength(1); j++)  
+            {  
+                row[j] = computers[i, j];  
+            }  
+
+            if (!row.Any(x => x == 1)) continue;  
+            bfs(row, i,computers);  
+            answer++;
+        }
+
+        return answer;
+    }
+    
+    public void bfs(int [] network, int idx, int[,] computers) {
+        for (int i = 0; i < network.Length;i++) {
+            if (network[i] == 0) continue;
+            
+            computers[idx, i] = 0;
+            
+            int[] next = new int[computers.GetLength(1)];  
+            for (int j = 0; j < computers.GetLength(1); j++) {  
+                next[j] = computers[i, j];  
+            }
+            bfs (next, i, computers);
+        }
+    }
+    
+//     public void checkLog(int [,] array) {
+//         for (int i = 0; i < array.GetLength(0);i++) {
+//             for (int j = 0; j < array.GetLength(1);j++) {
+//                 Console.Write(array[i,j]);
+//             }
+//             Console.WriteLine();
+//         }
+//         Console.WriteLine("---");
+//     }
+}
